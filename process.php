8f6fb9209abb8eb9d3a6160f6fd16497faccad89
Taylor Osbey <tosbey1@gmail.com>
2020-07-14 14:38:11 -0500

    I tried adding mysqli

--- process.php
+++ process.php
@@ -10,13 +10,13 @@
     $password = mysql_real_escape_string($password);
 
     // connect to the server and select database
-    mysql_connect("localhost","root", "");
-    mysql_select_db("login");
+    mysqli_connect("localhost","root", "");
+    mysqli_select_db("login");
 
     // Query the database for user
-    $result = mysql_query("select * from users where username = '$username' and password = '$password'") 
-                or die("Failed to query database ".mysql_error());
-    $row = mysql_fetch_array($result);
+    $result = mysqli_query("select * from users where username = '$username' and password = '$password'") 
+                or die("Failed to query database ".mysqli_error());
+    $row = mysqli_fetch_array($result);
     if ($row['username'] == $username && $row['password'] == $password)  {
         echo "Login success!! Welcome ".$row['username'];
     } else {
