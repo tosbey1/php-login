a6511125e2b05c4b1562448028c2f0ffb5b7cb82
Taylor Osbey <tosbey1@gmail.com>
2020-07-14 13:12:52 -0500

    initial commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="This is an example of a meta description.">
+        <meta name=viewport content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+
+
+        <header>
+            <nav>
+                <a href="#"></a>
+            </nav>
+        </header>
+
